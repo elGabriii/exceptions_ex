d8e86a8343a080cf5d7bf4d5d38a19b241b8ffbb
elGabri <elgabriii97@gmail.com>
2022-02-17 12:49:15 +0000

    Exercise completed with sucess

--- exception_ex/Program.cs
+++ exception_ex/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using exception_ex.Entities;
+using exception_ex.Entities.Exceptions;
+using System;
 
 namespace exception_ex
 {
@@ -6,7 +8,36 @@ namespace exception_ex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double ibalance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine());
+
+                Account acc = new Account(n, name, ibalance, limit);
+                Console.WriteLine();
+                Console.Write("Enter the amount to withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                acc.withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance);
+            }
+
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: "+e.Message);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("expected a number");
+            }
+
         }
+
     }
 }
