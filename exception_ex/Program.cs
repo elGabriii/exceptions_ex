102161ed77c2a137d40df911e73610c59ec57873
elGabri <elgabriii97@gmail.com>
2022-02-17 11:54:40 +0000

    Created the project

---
+++ exception_ex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exception_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
