c51969f727b59159ff0e66a59ca0e868cc1903ec
elGabri <elgabriii97@gmail.com>
2022-02-17 12:12:34 +0000

    Created the entitie Account

---
+++ exception_ex/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exception_ex.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
+
